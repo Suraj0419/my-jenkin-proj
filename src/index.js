33dfe484fdf10befb11191d5459af7b0a3ff72ce
Suraj0419 <suraj.pai@dhruvts.com>
2024-06-20 11:11:27 +0530

    typo fix/

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { loadConfig } from './configLoader'
+import { loadConfig } from './config.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 async function init() {
